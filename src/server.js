6b7724bf2e691a12e6ca16d2a3da3d4209fd16a9
S Dip <77108939+brblacky@users.noreply.github.com>
2021-07-28 12:35:20 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -472,62 +472,7 @@
         result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
     return result;
-}
-    app.get("/admin/news", checkMaintence, checkAdmin, checkAuth, async (req, res) => {
-        if(!config.bot.owners.includes(req.user.id)) return res.redirect('../admin');
-        const Database = require("void.db");
-        const db = new Database(path.join(__dirname, './database/json/news.json'));
-        renderTemplate(res, req, "/admin/administrator/news.ejs", { req, roles, config, db })
-    });
-    app.post("/admin/news", checkMaintence, checkAdmin, checkAuth, async (req, res) => {
-        if(!config.bot.owners.includes(req.user.id)) return res.redirect('../admin');
-        const Database = require("void.db");
-        const db = new Database(path.join(__dirname, './database/json/news.json'));
-        const datum =  new Date().toLocaleString();
-         db.push(`news`, { 
-           code: createID(12), 
-           icon: req.body.icon,
-           banner: 'https://cdn.discordapp.com/banners/844856727517003818/354c9f9cc32596ba24f4034bf53c8454.jpg?size=4096',
-           ownerID: req.user.id,
-           serverName: req.body.serverName,
-           color:  req.body.color,
-           rank:  req.body.rank,
-           date: datum,
-           description: req.body.partnerDesc
-         })
-         let rBody = req.body;
-        
-
-       const webhook = require("webhook-discord");
-  
-        const Hook = new webhook.Webhook("https://discord.com/api/webhooks/863599729657380865/pFmrUiOtjv-o-Fp0csX5pv-AoR6LuzvYUgrzR0tAGFKG_KMI5TV-3Ik_970iuErt517v");
-        const msg = new webhook.MessageBuilder()
-        .setName('Disbotlist | News')
-        .setAvatar(req.body.icon)
-        .setTitle(req.body.serverName)
-        .setDescription(`<@${req.user.id}> Posted a News \n\nLink:\n[website](https://disbotlist.xyz/news)`)
-        .setColor('#0099ff')
-        .setFooter(`Copyright © Disbotlist.xyz official 2021`)
-        Hook.send(msg);
-
-       
-         return res.redirect('/admin/news?success=true&message=News added.')
-         
-    });
-    function createID(length) {
-    var result = '';
-    var characters = '123456789';
-    var charactersLength = characters.length;
-    for (var i = 0; i < length; i++) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
-}
-    app.get("/news", checkMaintence, (req, res) => {
-        const Database = require("void.db");
-        const db = new Database(path.join(__dirname, './database/json/news.json'));
-      renderTemplate(res, req, "news.ejs", {roles, config, db,req: req});
-    });
+   });
     app.get("/admin/premium/delete/:botID", checkMaintence, checkAdmin, checkAuth, async (req, res) => {
         let rBody = req.body;
         await botsdata.findOneAndUpdate({
